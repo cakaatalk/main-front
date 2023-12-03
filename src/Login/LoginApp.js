aa8a5bbcc30d459acb98f7e70fbb2c24c00dea61
KimJinWooDa <76438011+KimJinWooDa@users.noreply.github.com>
2023-12-03 16:36:27 +0900

    [feat] 로그인 버튼 UI/UX 수정

--- src/Login/LoginApp.js
+++ src/Login/LoginApp.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import Icon from '../assets/ajou-icon.png';
 import '../css/components/loginPage.css';
 
 function LoginPage() {
   const navigate = useNavigate();
   const [isLoginView, setIsLoginView] = useState(true);
-  const [isTestMode, setIsTestMode] = useState(false);
+
+  const handleToggleView = () => {
+    setIsLoginView(!isLoginView);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,53 +19,45 @@ function LoginPage() {
       user_name: isLoginView ? null : username.value,
       password: password.value,
     };
-
-    if (!isTestMode) {
-      try {
-        if (isLoginView) {
-          await axios.post('http://localhost:8080/login', userData);
-        } else {
-          await axios.post('http://localhost:8080/signup', userData);
-        }
-        navigate('/friends');
-      } catch (error) {
-        console.error('An error occurred:', error);
-      }
-    } else {
-      console.log('Test Mode Active:', userData);
-      navigate('/friends');
+  
+    const endpoint = isLoginView ? '/api/auth/login' : '/api/auth/signup';
+    const apiUrl = `http://localhost:8080${endpoint}`;
+  
+    try {
+      await axios.post(apiUrl, userData);
+      navigate('/friends'); 
+    } catch (error) {
+      console.error('An error occurred:', error);
     }
   };
-
+  
   return (
-    <header className="welcome-header">
-      <img src={Icon} alt="Cocoa Icon" className="icon"/>
-      <h1 className="welcome-header__title">{isLoginView ? "CaKaA Talk 로그인" : "회원가입"}</h1>
-      <p className="welcome-header__text">
-        {isLoginView ? "아주대학교 이메일로 로그인 할 수 있습니다." : "새 계정을 생성하세요."}
-      </p>
-      <form onSubmit={handleSubmit} id="login-form">
-        {isLoginView ? (
-          <>
-            <input name="email" type="text" placeholder="email@ajou.ac.kr" required />
-            <input name="password" type="password" placeholder="비밀번호" required />
-          </>
-        ) : (
-          <>
-            <input name="email" type="text" placeholder="email@ajou.ac.kr" required />
-            <input name="username" type="text" placeholder="name" required />
-            <input name="password" type="password" placeholder="비밀번호" required />
-          </>
-        )}
-        <input type="submit" value={isLoginView ? "Log In" : "Sign Up"} />
-      </form>
-      <button onClick={() => setIsLoginView(!isLoginView)}>
-        {isLoginView ? "회원가입" : "로그인으로 돌아가기"}
-      </button>
-      <button onClick={() => setIsTestMode(!isTestMode)}>
-        {isTestMode ? "Disable Test Mode" : "Enable Test Mode"}
-      </button>
-    </header>
+    <div className="login-container">
+      <header className="welcome-header">
+        <h1 className="login-header">{isLoginView ? "CaKaA Talk 로그인" : "회원가입"}</h1>
+        <p className="login-description">
+          {isLoginView ? "아주대학교 이메일로 로그인 할 수 있습니다." : "새 계정을 생성하세요."}
+        </p>
+        <form onSubmit={handleSubmit} id="login-form">
+          {!isLoginView && (
+            <input name="username" type="text" placeholder="사용자 이름" className="input-username" required />
+          )}
+          <br></br>
+          <input name="email" type="text" placeholder="아주대학교 이메일(@ajou.ac.kr)" className="input-email" required />
+          <input name="password" type="password" placeholder="비밀번호" className="input-password" required />
+          <input type="submit" value={isLoginView ? "Log In" : "Sign Up"} className="login-button" />
+        </form>
+        <div className="additional-options">
+          <span onClick={handleToggleView} role="button" tabIndex={0} className="text-button">
+            {isLoginView ? "회원가입" : "로그인으로 돌아가기"}
+          </span>
+          <span className="text-separator">/</span>
+          <span onClick={() => { /* Logic for finding ID */ }} role="button" tabIndex={0} className="text-button">
+            아이디 찾기
+          </span>
+        </div>
+      </header>
+    </div>
   );
 }
 
