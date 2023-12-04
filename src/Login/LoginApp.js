0b7fb2e67a07c2c077f78d5bc75c1209c9d2b028
KimJinWooDa <76438011+KimJinWooDa@users.noreply.github.com>
2023-12-04 12:23:50 +0900

    [feat] 서버로부터 아이디 검색

--- src/Login/LoginApp.js
+++ src/Login/LoginApp.js
@@ -2,13 +2,34 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import '../css/components/loginPage.css';
+import WarningMessage from './WarningMessage'; 
 
 function LoginPage() {
   const navigate = useNavigate();
   const [isLoginView, setIsLoginView] = useState(true);
+  const [isFindIdView, setIsFindIdView] = useState(false);
+  const [isFindPasswordView, setIsFindPasswordView] = useState(false);
+  const [warningMessage, setWarningMessage] = useState('');
 
   const handleToggleView = () => {
     setIsLoginView(!isLoginView);
+    setIsFindIdView(false);
+    setIsFindPasswordView(false);
+    setWarningMessage('');
+  };
+
+  const handleFindIdView = () => {
+    setIsFindIdView(true);
+    setIsFindPasswordView(false);
+    setIsLoginView(false);
+    setWarningMessage('');
+  };
+
+  const handleFindPasswordView = () => {
+    setIsFindPasswordView(true);
+    setIsFindIdView(false);
+    setIsLoginView(false);
+    setWarningMessage('');
   };
 
   const handleSubmit = async (e) => {
@@ -25,35 +46,49 @@ function LoginPage() {
   
     try {
       await axios.post(apiUrl, userData);
-      navigate('/friends'); 
+      navigate('/friends');
     } catch (error) {
-      console.error('An error occurred:', error);
+      isLoginView ? setWarningMessage('ID가 없습니다.') : setWarningMessage('이미 존재하는 ID입니다.');
     }
+    
   };
   
+  if (isFindIdView) {
+    return <div>ID 찾기</div>;
+  }
+
+  if (isFindPasswordView) {
+    return <div>PWD 찾기</div>;
+  }
+
   return (
     <div className="login-container">
       <header className="welcome-header">
         <h1 className="login-header">{isLoginView ? "CaKaA Talk 로그인" : "회원가입"}</h1>
         <p className="login-description">
-          {isLoginView ? "아주대학교 이메일로 로그인 할 수 있습니다." : "새 계정을 생성하세요."}
+          {isLoginView ? "You can log in with your Ajou University email address." : "새로운 계정을 만드세요."}
         </p>
         <form onSubmit={handleSubmit} id="login-form">
           {!isLoginView && (
-            <input name="username" type="text" placeholder="사용자 이름" className="input-username" required />
+            <input name="username" type="text" placeholder="username" className="input-username" required />
           )}
           <br></br>
-          <input name="email" type="text" placeholder="아주대학교 이메일(@ajou.ac.kr)" className="input-email" required />
-          <input name="password" type="password" placeholder="비밀번호" className="input-password" required />
-          <input type="submit" value={isLoginView ? "Log In" : "Sign Up"} className="login-button" />
+          <input name="email" type="text" placeholder="Ajou University Email (@ajou.ac.kr)" className="input-email" required />
+          <input name="password" type="password" placeholder="password" className="input-password" required />
+          <input type="submit" value={isLoginView ? "로그인" : "회원가입"} className="login-button" />
         </form>
+        {warningMessage && <WarningMessage message={warningMessage} />}
         <div className="additional-options">
           <span onClick={handleToggleView} role="button" tabIndex={0} className="text-button">
             {isLoginView ? "회원가입" : "로그인으로 돌아가기"}
           </span>
           <span className="text-separator">/</span>
-          <span onClick={() => { /* Logic for finding ID */ }} role="button" tabIndex={0} className="text-button">
+          <span onClick={handleFindIdView} role="button" tabIndex={0} className="text-button">
             아이디 찾기
+          </span>
+          <span className="text-separator">/</span>
+          <span onClick={handleFindPasswordView} role="button" tabIndex={0} className="text-button">
+            패스워드 찾기
           </span>
         </div>
       </header>
