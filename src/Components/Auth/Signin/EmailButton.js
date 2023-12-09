1bd5613a1cb810808c2f1b5eec07e79544c7daf7
Donghyun Kim <ilovekdh1208@ajou.ac.kr>
2023-12-10 00:56:37 +0900

    feat: disable 로직 추가, 스타일 추가

--- src/Components/Auth/Signin/EmailButton.js
+++ src/Components/Auth/Signin/EmailButton.js
@@ -1,79 +1,81 @@
 import React, { useState } from "react";
 
 function EmailButton({
-  onClick,
-  text,
-  email,
-  setWarningMessage,
-  setViewable,
-  disableValue,
+    onClick,
+    text,
+    email,
+    setWarningMessage,
+    setViewable,
+    disableValue,
+    setDisableValue,
 }) {
-  const [verifyNum, setVerifyNum] = useState("");
-  const [verificationComplete, setVerificationComplete] = useState(false);
-  const [verifyText, setVerifyText] = useState("인증");
+    const [verifyNum, setVerifyNum] = useState("");
+    const [verificationComplete, setVerificationComplete] = useState(false);
+    const [verifyText, setVerifyText] = useState("인증");
 
-  const onValueChange = (e) => {
-    setWarningMessage(``);
-    setVerifyNum(e.target.value);
-  };
+    const onValueChange = (e) => {
+        setWarningMessage(``);
+        setVerifyNum(e.target.value);
+    };
 
-  const handleCheckboxClick = async () => {
-    setWarningMessage(``);
+    const handleCheckboxClick = async () => {
+        setWarningMessage(``);
 
-    try {
-      const response = await fetch(
-        "http://localhost:8080/api/auth/mail/verify",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email: email,
-            authCode: verifyNum,
-          }),
+        try {
+            const response = await fetch(
+                "http://localhost:8080/api/auth/mail/verify",
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        email: email,
+                        authCode: verifyNum,
+                    }),
+                }
+            );
+
+            if (response.ok) {
+                setVerificationComplete(true);
+                setDisableValue(true);
+                setVerifyText("인증 완료");
+                setViewable("");
+            } else {
+                const errorResponse = await response.json();
+                setWarningMessage(errorResponse.error);
+            }
+        } catch (error) {
+            setWarningMessage(`${error.response.data.error}`);
         }
-      );
+    };
 
-      if (response.ok) {
-        setVerificationComplete(true);
-        setVerifyText("인증 완료");
-        setViewable("");
-      } else {
-        const errorResponse = await response.json();
-        setWarningMessage(errorResponse.error);
-      }
-    } catch (error) {
-      setWarningMessage(`${error.response.data.error}`);
-    }
-  };
-
-  return (
-    <div className="email-verification-div">
-      <button
-        type="button"
-        className="email-button"
-        onClick={onClick}
-        disabled={disableValue}
-      >
-        {text}
-      </button>
-      <input
-        type="text"
-        value={verifyNum}
-        onChange={onValueChange}
-        placeholder="Verification Number"
-        className="email-verification-input"
-      />
-      <input
-        type="button"
-        className="email-check-button"
-        onClick={handleCheckboxClick}
-        value={verifyText}
-        disabled={verificationComplete}
-      />
-    </div>
-  );
+    return (
+        <div className="email-verification-div">
+            <button
+                type="button"
+                className={disableValue ? "email-button disabled-button" : "email-button"}
+                onClick={onClick}
+                disabled={disableValue}
+            >
+                {text}
+            </button>
+            <input
+                type="text"
+                value={verifyNum}
+                onChange={onValueChange}
+                placeholder="Verification Number"
+                className="email-verification-input"
+            />
+            <input
+                type="button"
+                className={verificationComplete ? "email-check-button disabled-button" : "email-check-button"}
+                onClick={handleCheckboxClick}
+                value={verifyText}
+                disabled={verificationComplete}
+            />
+        </div>
+    );
 }
 
 export { EmailButton };
